385469727ca668694f31eb48e6315b877bb29608
Renan Pinto <renanrnpinto@gmail.com>
2022-07-20 17:04:05 -0300

    Requisito 5 - lint atualizado

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
-    const { planetName, planetImage} = this.props;
+    const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-      <p data-testid="planet-name">
-        {planetName}
-      </p>
-      <img src={planetImage} alt= {`Planeta ${planetName}`} />
+        <p data-testid="planet-name">
+          {planetName}
+        </p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
-    )
+    );
   }
 }
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanetCard;
