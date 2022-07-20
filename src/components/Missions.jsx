d120eb8d22c2a7221e100c5054f47ffd7fb13c56
Renan Pinto <renanrnpinto@gmail.com>
2022-07-20 17:52:32 -0300

    Requisito 8

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions" />
+      <div data-testid="missions">
+        <p><Title headline="Missões" /></p>
+      </div>
     );
   }
 }
